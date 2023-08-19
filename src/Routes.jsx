651811162d06f1ea5adaec69820d5ba3f38e9a54
Anomalyforlife <themrsimon1@gmail.com>
2023-08-19 13:50:33 +0200

    build: :heavy_plus_sign: added chakra icons

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+
 function App() {
 
   return (
-    <>
-      <div>
-        HI
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route index element={<Landing />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
